70b7379902f9d83010df844364eeef2db51bad2d
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-02-16 15:50:16 +0100

    Refactor code
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- src/cmd/transpose/lift_to_workspace.rs
+++ src/cmd/transpose/lift_to_workspace.rs
@@ -20,7 +20,7 @@ pub struct LiftToWorkspaceCmd {
 	cargo_args: CargoArgs,
 
 	#[clap(index(1))]
-	dependency: String,
+	dependencies: Vec<String>,
 
 	/// Instead of dry-running, actually modify the files.
 	#[clap(long)]
@@ -51,28 +51,134 @@ pub enum VersionResolveMode {
 impl LiftToWorkspaceCmd {
 	pub fn run(&self, g: &GlobalArgs) -> Result<(), String> {
 		g.warn_unstable();
+		self.validate_args()?;
 
+		let meta = self.cargo_args.clone().with_workspace(true).load_metadata()?;
+		let mut fixers = Map::new();
+
+		for dep in &self.dependencies {
+			self.run_for_dependency(g, &meta, dep, &mut fixers)?;
+		}
+
+		self.try_apply_changes(&mut fixers)
+	}
+
+	fn validate_args(&self) -> Result<(), String> {
 		if self.exact_version.is_some() && self.version_resolver != VersionResolveMode::Exact {
 			return Err("Cannot use --exact-version without --version-resolver=exact".to_string())
 		}
+		Ok(())
+	}
 
-		let meta = self.cargo_args.clone().with_workspace(true).load_metadata()?;
-		log::debug!("Scanning workspace for '{}'", self.dependency);
-		// version -> crate
-		let mut by_version = HashMap::<semver::VersionReq, Vec<(Package, Dep)>>::new();
+	fn try_apply_changes(
+		&self,
+		fixers: &mut Map<String, (Option<Package>, AutoFixer)>,
+	) -> Result<(), String> {
+		let mut modified = 0;
+		for (_pkg, fixer) in fixers.values_mut() {
+			if !fixer.modified() {
+				continue
+			}
 
+			modified += 1;
+			if self.fix {
+				fixer.save()?;
+			} else if let Some(_pkg) = _pkg {
+				log::debug!("Would modify {:?}", _pkg.name);
+			} else {
+				log::debug!("Would modify the workspace");
+			}
+		}
+		if modified > 0 && !self.fix {
+			let s = plural(modified);
+			Err(format!(
+				"Held back modifications to {modified} file{s}. Re-run with --fix to apply."
+			))
+		} else {
+			Ok(())
+		}
+	}
+
+	fn run_for_dependency(
+		&self,
+		g: &GlobalArgs,
+		meta: &cargo_metadata::Metadata,
+		dep: &str,
+		fixers: &mut Map<String, (Option<Package>, AutoFixer)>,
+	) -> Result<(), String> {
+		let by_version = self.build_version_index(&meta, dep);
+		let versions = by_version.keys().collect::<Vec<_>>();
+		let best_version = self.find_best_version(g, dep, &versions, &by_version)?;
+
+		log::info!(
+			"Selected '{} {}'", //: N={}, D={}, B={})",
+			dep,
+			&best_version,
+		);
+
+		for (pkg, dep) in by_version.values().flatten() {
+			let krate_path = canonicalize(pkg.manifest_path.clone().into_std_path_buf()).unwrap();
+			let allowed_dir = canonicalize(meta.workspace_root.as_std_path()).unwrap();
+
+			if !krate_path.starts_with(&allowed_dir) {
+				log::info!(
+					"Skipping path outside of the workspace: {:?} (not in {:?})",
+					krate_path.display(),
+					allowed_dir.display()
+				);
+				continue
+			}
+
+			fixers.entry(pkg.name.clone()).or_insert_with(|| {
+				(Some(pkg.clone()), AutoFixer::from_manifest(&krate_path).unwrap())
+			});
+			let (_, fixer) = fixers.get_mut(&pkg.name).unwrap();
+
+			let default_feats = dep.uses_default_features.then_some(true);
+			fixer.lift_dependency(&dep.name, default_feats)?;
+		}
+
+		// Now create fixer for the root package
+		let root_manifest_path = meta.workspace_root.join("Cargo.toml");
+		let mut workspace_fixer =
+			AutoFixer::from_manifest(&root_manifest_path.into_std_path_buf())?;
+
+		let mut dep = by_version.values().next().unwrap().first().unwrap().1.clone();
+		dep.req = best_version.parse().unwrap();
+		// We always add `default-features = false` into the workspace:
+		workspace_fixer.add_workspace_dep(&dep, false)?;
+		fixers.insert("workspace".to_string(), (None, workspace_fixer));
+
+		Ok(())
+	}
+
+	/// Index what versions of a crate are used in the workspace.
+	fn build_version_index(
+		&self,
+		meta: &cargo_metadata::Metadata,
+		name: &str,
+	) -> HashMap<VersionReq, Vec<(Package, Dep)>> {
+		let mut by_version = HashMap::<VersionReq, Vec<(Package, Dep)>>::new();
 		for pkg in meta.packages.iter() {
 			for dep in pkg.dependencies.iter() {
-				if dep.name != self.dependency {
+				if dep.name != name {
 					continue
 				}
 
 				by_version.entry(dep.req.clone()).or_default().push((pkg.clone(), dep.clone()));
 			}
 		}
+		by_version
+	}
 
-		let versions = by_version.keys().collect::<Vec<_>>();
-		let best_version = match self.version_resolver {
+	fn find_best_version(
+		&self,
+		g: &GlobalArgs,
+		name: &str,
+		versions: &Vec<&VersionReq>,
+		by_version: &HashMap<VersionReq, Vec<(Package, Dep)>>,
+	) -> Result<String, String> {
+		let found = match self.version_resolver {
 			VersionResolveMode::Exact => self.exact_version.clone().expect("Checked by clippy"),
 			VersionResolveMode::Latest => try_find_latest(by_version.keys())?.to_string(),
 			VersionResolveMode::Unambiguous => {
@@ -108,11 +214,11 @@ impl LiftToWorkspaceCmd {
 						},
 					};
 
-					let hint = format!("cargo upgrade -p {}@{version_hint}", &self.dependency);
+					let hint = format!("cargo upgrade -p {}@{version_hint}", name);
 					return Err(format!(
 						"\nFound {} different versions of '{}' in the workspace:\n\n{err}\nHint: {}\n",
 						versions.len(),
-						&self.dependency,
+						name,
 						g.bold(&hint),
 					))
 				} else {
@@ -120,74 +226,7 @@ impl LiftToWorkspaceCmd {
 				}
 			},
 		};
-
-		log::info!(
-			"Selected '{} {}'", //: N={}, D={}, B={})",
-			&self.dependency,
-			&best_version,
-		);
-
-		let mut fixers = Map::new();
-		for (pkg, dep) in by_version.values().flatten() {
-			let krate_path = canonicalize(pkg.manifest_path.clone().into_std_path_buf()).unwrap();
-			let allowed_dir = canonicalize(meta.workspace_root.as_std_path()).unwrap();
-
-			if !krate_path.starts_with(&allowed_dir) {
-				log::info!(
-					"Skipping path outside of the workspace: {:?} (not in {:?})",
-					krate_path.display(),
-					allowed_dir.display()
-				);
-				continue
-			}
-
-			fixers
-				.entry(pkg.name.clone())
-				.or_insert_with(|| (pkg, AutoFixer::from_manifest(&krate_path).unwrap()));
-			let (_, fixer) = fixers.get_mut(&pkg.name).unwrap();
-
-			let default_feats = dep.uses_default_features.then_some(true);
-			fixer.lift_dependency(&dep.name, default_feats)?;
-		}
-
-		// Now create fixer for the root package
-		let root_manifest_path = meta.workspace_root.join("Cargo.toml");
-		let mut fixer = AutoFixer::from_manifest(&root_manifest_path.into_std_path_buf())?;
-		let mut dep = by_version.values().next().unwrap().first().unwrap().1.clone();
-		dep.req = best_version.parse().unwrap();
-		// We always add `default-features = false` into the workspace:
-		fixer.add_workspace_dep(&dep, false)?;
-
-		let mut modified = 0;
-		for (_pkg, fixer) in fixers.values_mut() {
-			if !fixer.modified() {
-				continue
-			}
-
-			modified += 1;
-			if self.fix {
-				fixer.save()?;
-			} else {
-				log::debug!("Would modify {:?}", _pkg.name);
-			}
-		}
-		if fixer.modified() {
-			modified += 1;
-			log::debug!("Would modify the workspace");
-
-			if self.fix {
-				fixer.save()?;
-			}
-		}
-
-		if modified > 0 && !self.fix {
-			let s = plural(modified);
-			Err(format!(
-				"Held back modifications to {modified} file{s}; re-run with --fix to apply."
-			))
-		} else {
-			Ok(())
-		}
+		Ok(found)
 	}
 }
 
